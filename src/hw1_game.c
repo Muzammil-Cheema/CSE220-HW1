861a64a8c688b3f1c6d22b060ee502ba010e4228
Muzammil-Cheema <muzi1747@gmail.com>
2025-02-21 22:08:33 +0000

    Fixed exit code 1 error on invalid initial board state.

--- src/hw1_game.c
+++ src/hw1_game.c
@@ -11,7 +11,7 @@ int main(int argc, char **argv) {
     assert(argc == 4); //we want a size, board state, and a list of keys
 	if (initialize_board(argv[2], argv[3], (int)strtoul(argv[1], NULL, 10)) == 0){
         printf("Invalid initial board state.\n");
-        return 1;
+        return 0;
     }	
     game();
     return 0;
